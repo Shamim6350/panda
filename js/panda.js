39fa73b4f92f8747c382a3b8c48b25f3a11332da
shamim6350 <kshamim00@gmail.comm>
2022-02-09 00:16:26 +0600

    full

---
+++ js/panda.js
@@ -0,0 +1,6 @@
+// const headernName = document.getElementsByTagName('h1');
+// // console.log(headernName);
+// for (const header of headernName) {
+//     header.style.backgroundColor = 'lightblue';
+// }
+
